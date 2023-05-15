9b1793f8b6764bb404be1c68ac8d38c2ccf90f00
Joe Fitzsimmons <jfitzsimmons2@gmail.com>
2023-05-15 00:04:35 -0400

    global config cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,22 @@
 import Card from "primevue/card";
 import HelloWorld from "./components/HelloWorld.vue";
 import { usePrimeVue } from "primevue/config";
-
 import Accordion from "primevue/accordion";
 import AccordionTab from "primevue/accordiontab";
 import { computed } from "vue";
-import { AccordionPassThroughAttributes } from "primevue/accordion";
 
 const pv = usePrimeVue();
-
 const passThrough = computed(() => pv.config.pt);
 
-const accordionPT: AccordionPassThroughAttributes = {
-  root: ({ props, parent }) => ({
-    class: "border border-1 border-gray-200 overflow-hidden shadow-sm root",
-    test: props,
-  }),
-  header: ({ props, parent }) => ({
-    class: parent.state.d_activeIndex === 0 ? "active" : "inactive",
-  }),
-  headerAction: {
-    class: "block p-4 bg-gray-100 text-gray-800 font-bold",
-  },
-  content: {
-    class: "p-4 bg-white text-gray-800 font-bold",
-  },
-};
-if (passThrough.value) {
-  passThrough.value.accordion = accordionPT;
-  console.log(passThrough.value.accordion);
-  debugger;
-}
+console.log("passThrough", passThrough.value);
 </script>
 
 <template>
-  <Card style="max-width: 30rem" class="mx-auto">
+  <Card class="mx-auto my-4">
+    <template #header> Card component </template>
     <template #content>
-      <div class="text-center">
-        <div class="flex">
+      <div class="text-center w-full">
+        <div class="flex justify-center">
           <a href="https://vitejs.dev" target="_blank">
             <img src="/vite.svg" class="logo" alt="Vite logo" />
           </a>
@@ -57,15 +36,45 @@ if (passThrough.value) {
       </div>
     </template>
   </Card>
-  <pre>
-		{{ passThrough }}
-	</pre
-  >
-  <Accordion>
-    <AccordionTab header="what">Wow</AccordionTab>
-    <AccordionTab header="what">Wow</AccordionTab>
-    <AccordionTab header="what">Wow</AccordionTab>
-  </Accordion>
+
+  <div class="sm:grid sm:grid-cols-2">
+    <Card class="w-full">
+      <template #header> Accordion component </template>
+      <template #content>
+        <Accordion>
+          <AccordionTab header="what"
+            >Reprehenderit voluptate Lorem ut exercitation irure excepteur ut
+            culpa commodo. Ad tempor ut esse sit duis ad voluptate tempor.
+            Exercitation proident fugiat ut minim incididunt aliqua non pariatur
+            minim ea officia. Fugiat mollit quis sunt ex Lorem adipisicing.
+            Incididunt esse culpa laborum pariatur officia non anim et. Dolor
+            reprehenderit dolore dolor minim reprehenderit.</AccordionTab
+          >
+          <AccordionTab header="what"
+            >Ullamco sint laborum nisi magna eu cupidatat laboris laborum sunt
+            irure aute exercitation. Quis aute culpa dolore sunt ipsum laboris
+            nulla ipsum veniam veniam. Duis elit tempor voluptate elit et culpa
+            pariatur mollit anim nisi nisi enim reprehenderit. Veniam duis enim
+            pariatur exercitation laborum veniam elit ut excepteur ullamco irure
+            irure magna. Laboris ad commodo minim eu qui adipisicing pariatur
+            dolore magna ea nostrud. Nostrud qui est ad nulla consequat
+            consequat consequat cillum. Elit reprehenderit eiusmod elit
+            commodo.</AccordionTab
+          >
+          <AccordionTab header="what"
+            >Dolor aute esse elit aliquip veniam eu. Et culpa culpa occaecat
+            consectetur irure commodo amet. Sunt commodo eiusmod qui excepteur
+            est irure aute labore. Adipisicing consectetur consectetur tempor
+            incididunt aliqua ut aliqua labore aute commodo tempor pariatur
+            mollit veniam. Exercitation cupidatat anim enim dolor anim fugiat
+            est commodo consectetur id ut adipisicing. Excepteur dolore sint
+            tempor minim enim amet. Nisi do deserunt pariatur excepteur
+            consequat Lorem commodo nisi.</AccordionTab
+          >
+        </Accordion>
+      </template>
+    </Card>
+  </div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ const app = createApp(App);
 app.use(PrimeVue, {
 	ripple: true,
 	pt: {
+		accordion: {
+			root: {
+				class: "border border-1 border-gray-200 overflow-hidden shadow-sm root",
+			},
+			header: ({ props, parent }) => ({
+				class: parent.state.d_activeIndex === 0 ? "active" : "inactive",
+			}),
+			headerAction: {
+				class: "block p-4 bg-gray-100 text-gray-800 font-bold",
+			},
+			content: {
+				class: "p-4 bg-white text-gray-800 font-bold",
+			},
+		},
 		panel: {
 			// Panel Pass Through Options: https://primevue.org/panel
 			root: { style: { fontWeight: "bold" } },
@@ -23,8 +37,10 @@ app.use(PrimeVue, {
 			title: { class: "text-white" },
 		},
 		card: {
-			// Card Pass Through Options: https://primevue.org/card
-			body: { class: "bg-cyan-300 border-round-lg" },
+			// Card Pass Through Options: https://primevue.org/card 
+			root: { class: 'block bg-white border border-gray-100 rounded shadow-md hover:shadow transition-shadow', },
+			header: { class: "font-bold uppercase tracking-wide text-gray-700 border-b p-4 bg-slate-50" },
+			body: { class: "p-4" },
 		},
 	},
 });
